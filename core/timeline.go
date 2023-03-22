32e34ae8f1f8e473c28c77e36e1ab93ed8668ad8
Brian <brianbwheeler@gmail.com>
2023-03-22 17:26:47 -0400

    naming improvements

--- core/timeline.go
+++ core/timeline.go
@@ -2,9 +2,9 @@ package core
 
 import "fmt"
 
-const timelineEntity string = "ENTITY_TIMELINE"
-const currentTick string = "CURRENT_TICK"
-const actionStartTick string = "ACTION_START_TICK"
+const timelineEntityID string = "ENTITY_TIMELINE"
+const currentTickAttribute string = "CURRENT_TICK"
+const actionStartTickAttribute string = "ACTION_START_TICK"
 
 
 type Timeline struct {
@@ -32,16 +32,16 @@ func (t *Timeline) AddAction(action *Action) error {
 	if err != nil {
 		return err
 	}
-	return action.SetAttribute(actionStartTick, tick)
+	return action.SetAttribute(actionStartTickAttribute, tick)
 }
 
 func (t *Timeline) RemoveAction(action *Action) error {
-	return action.RemoveAttribute(actionStartTick)
+	return action.RemoveAttribute(actionStartTickAttribute)
 }
 
 
 func (t *Timeline) GetCurrentTick() (int64, error) {
-	timelineEntities, err := t.dao.GetEntitiesWithAttributeType(currentTick)
+	timelineEntities, err := t.dao.GetEntitiesWithAttributeType(currentTickAttribute)
 	if err != nil {
 		 return 0, err
 	}
@@ -50,10 +50,10 @@ func (t *Timeline) GetCurrentTick() (int64, error) {
 	}
 	if len(timelineEntities) < 1 {
 		timelineEntity := NewEntity(t.dao)
-		timelineEntity.SetAttribute(currentTick, 0)
+		timelineEntity.SetAttribute(currentTickAttribute, 0)
 		timelineEntities = append(timelineEntities, timelineEntity)
 	}
-	currentTick, err := timelineEntities[0].GetAttribute(currentTick)
+	currentTick, err := timelineEntities[0].GetAttribute(currentTickAttribute)
 	return currentTick.(int64), err
 }
 
@@ -82,7 +82,7 @@ func (t *Timeline) GetPendingAction() (*Action, error) {
 }
 
 func (t *Timeline) GetStartTickOfAction(a *Action) (int64, error) {
-	startTick, err := a.GetAttribute(actionStartTick)
+	startTick, err := a.GetAttribute(actionStartTickAttribute)
 	if startTick == nil {
 		return 0, fmt.Errorf("Action %v has no start tick", a)
 	}
@@ -101,7 +101,7 @@ func (t *Timeline) GetEndTickOfAction(a *Action) (int64, error) {
 }
 
 func (t *Timeline) SetCurrentTick(tick int64) error {
-	return t.dao.SetAttribute(timelineEntity, currentTick, tick)
+	return t.dao.SetAttribute(timelineEntityID, currentTickAttribute, tick)
 }
 
 
@@ -115,7 +115,7 @@ func indexOfAction(action *Action, actions []*Action) (int, error) {
 }
 
 func getActions(dao EntityDao) ([]*Action, error) {
-	actionEntities, err := dao.GetEntitiesWithAttributeType(actionStartTick)
+	actionEntities, err := dao.GetEntitiesWithAttributeType(actionStartTickAttribute)
 	if err != nil {
 		return nil, err
 	}
